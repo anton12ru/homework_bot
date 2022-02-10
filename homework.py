3faae2103d4dc3b100e11a55886e2feea820b51e
Антон Булыгин <kampris2013@yandex.ru>
2022-02-10 21:32:24 +0300

    Second editing after review

--- homework.py
+++ homework.py
@@ -37,7 +37,7 @@ logging.basicConfig(
     level=logging.DEBUG,
     filename=os.path.expanduser('~/main.log'),
     encoding='UTF-8',
-    filemode='a',
+    filemode='w',
 )
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -48,7 +48,7 @@ logger.addHandler(handler)
 def send_message(bot, message):
     """Отправляет сообщение."""
     try:
-        bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+        bot.send_message(TELEGRAM_CHAT_ID, message)
         logging.info(f'отправлено сообщение: "{message}"')
     except Exception:
         logging.error('Не было доставлено сообщение в чат')
@@ -105,12 +105,16 @@ def parse_status(homework):
     """Функция извлекает из информации о конкретной домашней работе.
     статус этой работы.
     """
-    if 'homework_name' not in homework:
-        raise KeyError('Отсутствует ключ "homework_name" в ответе API')
-    if 'status' not in homework:
-        raise TypeError('Отсутствует ключ "status" в ответе API')
     homework_name = homework['homework_name']
+    if homework_name is None:
+        logging.error(
+            f'ключ {homework_name}, не соотвествует ключу "homework_name"')
+        raise TypeError('Отсутствует ключ "homework_name" в ответе API')
     homework_status = homework['status']
+    if homework_status is None:
+        logging.error(
+            f'ключ {homework_status}, не соотвествует ключу "status"')
+        raise KeyError('Отсутствует ключ "status" в ответе API')
     if homework_status not in HOMEWORK_STATUSES:
         message = f'ключ {homework_status} не найден'
         logging.error(message)
@@ -126,27 +130,26 @@ def check_tokens():
         'TELEGRAM_TOKEN': TELEGRAM_TOKEN,
         'TELEGRAM_CHAT_ID': TELEGRAM_CHAT_ID,
     }
+
     try:
         if all(ALL_TOKENS.values()):
             return True
     except Exception:
-        for tokens_name in ALL_TOKENS.items():
-            logger.critical(
-                f'Отсутствует обязательная переменная окружения: {tokens_name}'
-            )
+        if ALL_TOKENS.values() is None:
+            for token_name, token in ALL_TOKENS.items():
+                logger.critical(
+                    'Отсутствует обязательная переменная окружения:'
+                    f'{token} в токене {token_name}')
             return False
 
 
 def main():
     """Основная логика работы бота."""
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    bot = telegram.Bot(TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
-    # if not check_tokens():
-    #     logger.critical(
-    #         'Проверить наличие переменных окружения в .env'
-    #     )
-    #     raise Exception('Проверить наличие переменных окружения в .env')
-    check_tokens()
+    if not check_tokens():
+        exit()
+
     while True:
         try:
             response = get_api_answer(current_timestamp)
@@ -154,7 +157,6 @@ def main():
             if homework:
                 parse_status_result = parse_status(homework)
                 send_message(bot, parse_status_result)
-            current_timestamp = response.get('current_date', current_timestamp)
             time.sleep(RETRY_TIME)
 
         except Exception as error:
